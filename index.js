953e95faaf7bc37cbd9e0138ff9b35d1d42280b9
KI-Anik <iamkhairul101@gmail.com>
2025-02-12 21:16:05 +0600

    implement vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const campCollection = client.db('a10-fundingDB').collection('campaigns')
         const donarCollection = client.db('a10-fundingDB').collection('Donars')
